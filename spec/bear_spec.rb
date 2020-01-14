386e0b6adf5d94359ec1fbae2c215807be6b5deb
onitwin <onitwin@gmail.com>
2020-01-14 16:02:37 +0000

    creating files with no content

---
+++ spec/bear_spec.rb
@@ -0,0 +1,9 @@
+require("minitest/autorun")
+require("minitest/reporters")
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+require_relative('../bear')
+
+
+
+class BearTest < MiniTest::Test
